dbce2dba302a0c8f14aadaab6f570dfd67084926
Peter Szucs <pszucs@outlook.com>
2015-01-29 14:08:14 +0100

    first 8 problems without primes

---
+++ Euler/Program.fs
@@ -0,0 +1,11 @@
+﻿// Learn more about F# at http://fsharp.net
+// See the 'F# Tutorial' project for more help.
+open Euler.Tier1
+open System
+
+[<EntryPoint>]
+let main argv = 
+    printfn "%d" problem1
+    
+    Console.Read()
+    0 // return an integer exit code
---
+++ Euler/Tier1.fs
@@ -0,0 +1,14 @@
+﻿namespace Euler
+
+    module Tier1 = 
+        
+        let problem1 =
+            [1 .. 999] |> List.filter(fun x -> x % 3 = 0 || x % 5 = 0) |> List.fold(fun acc x -> acc + x) 0 
+
+        let problem2 =
+            let rec fibutil num state idx targetIdx = 
+                match num with 
+                | 1 -> 1
+                | 2 -> 2
+                | _ -> 0
+            0
